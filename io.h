596c8f66eba6fba8b19c9de36d267770f3e32c66
AutisticNinja <gregwood24@gmail.com>
2023-08-27 10:50:32 -0600

    start of PIO support

--- io.h
+++ io.h
@@ -1,5 +1,6 @@
 #define UART_TX_GPIO 8
 #define UART_RX_GPIO 9
 #define PPM_OUTPUT_PIN 15
+#define USB_POWER_PIN 14
 
 #define TEST_GPIO 16
--- tusb_config.h
+++ tusb_config.h
@@ -47,7 +47,7 @@
 
 #if CFG_TUSB_MCU == OPT_MCU_RP2040
 // change to 1 if using pico-pio-usb as host controller for raspberry rp2040
-#define CFG_TUH_RPI_PIO_USB   0
+#define CFG_TUH_RPI_PIO_USB   1
 #define BOARD_TUH_RHPORT      CFG_TUH_RPI_PIO_USB
 #endif
 
